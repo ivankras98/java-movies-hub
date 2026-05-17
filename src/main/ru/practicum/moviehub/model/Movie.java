2f93532f731ee6427963546e9ec2fc1defe9d314
Ivan Krasilnikiov <ikra-nn@yandex.ru>
2026-05-17 20:18:12 +0300

    Внесены изменения

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -9,8 +9,19 @@ public class Movie {
         this.name = name;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
